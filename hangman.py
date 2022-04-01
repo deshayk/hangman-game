daf00451acbc7b8bc4b81fd17f81cc789cad9c02
deshayk <deshaykidd@gmail.com>
2022-04-01 12:52:48 -0500

    update doc description'

--- hangman.py
+++ hangman.py
@@ -1,2 +1,9 @@
+"""
+Learn Python the Fun Way - Hangman Game by DeShay K.
+Github: https://github.com/deshayk
+Medium: https://www.medium.com/@deshayk
+LinkedIn: https://www.linkedin.com/in/deshayk/
+"""
+
 import random # gives you access to the random module, needed to choose a random word from the word-list python file 
 from wordList import words # imports the words array file from the same directory as the wordList.py file
